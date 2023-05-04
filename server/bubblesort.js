53d89298f5ae7527531abb83864ff94b7d734bd7
Carlos Ribeiro <carlos.ribeiro@ccc.ufcg.edu.br>
2023-05-04 11:14:07 -0300

    server: add new route for sorting

---
+++ server/bubblesort.js
@@ -0,0 +1,17 @@
+function bubblesort(array) {
+    for (var i = 0; i < array.length; i++) {
+        for (var j = 0; j < (array.length - i - 1); j++) {
+            if (array[j] > array[j + 1]) {
+                var temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+            }
+        }
+    }
+    
+    return array;
+}
+
+module.exports = {
+    bubblesort: bubblesort,
+};
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bubblesort = require('./bubblesort.js').bubblesort;
 
 const app = express();
 app.use(express.json());
@@ -6,9 +7,15 @@ app.use(express.json());
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-app.post('/', (req, res) => {
+app.post('/quicksort', (req, res) => {
   let unsorted = req.body['array'];
   let result = { sorted: unsorted.sort((i, j) => i-j) };
+  res.status(200).send(result);
+});
+
+app.post('/bubblesort', (req, res) => {
+  let unsorted = req.body['array'];
+  let result = { sorted: bubblesort(unsorted) };
   res.status(200).send(result);
 });
 
